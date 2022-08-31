96c57a09ea765cb00e244976bc7b35d065b143a0
Arktic-Panda <65855745+Arktic-Panda@users.noreply.github.com>
2022-08-31 11:19:17 +0100

    New Update

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,22 +7,29 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefab;
     private float xRange = 9.8f;
     private float zRange = 30;
+    public float spawnDelay;
+    public float spawnInterval;
 
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        //Spawning animals with a delay and at intervals on launch
+        InvokeRepeating("SpawnRandomAnimal", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefab.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(xRange,-xRange), 0, zRange);
-            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
-        }
+        
+    }
+
+    //Spawn animal method
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefab.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(xRange,-xRange), 0, zRange);
+
+        Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
     }
 }
